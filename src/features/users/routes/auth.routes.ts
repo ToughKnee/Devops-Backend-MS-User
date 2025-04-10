c1b256455fe5f6825e5aaac908c819d884cf9145
AngelChaves <angel.chaveschinchilla@ucr.ac.cr>
2025-04-09 20:03:51 -0600

    chore(mvc firebase login): implements mvc patter with a router, controller and a service to check the user token

---
+++ src/features/users/routes/auth.routes.ts
@@ -0,0 +1,9 @@
+// src/features/users/routes/auth.routes.ts
+import { Router } from 'express';
+import { loginUser } from '../controllers/auth.controller';
+
+const router = Router();
+
+router.post('/login', loginUser);
+
+export default router;
