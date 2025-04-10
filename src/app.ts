4fbd6924465627caa0b302485fcc40960311d9b3
Johana Wu Nie <johana.wu@ucr.ac.cr>
2025-04-10 14:28:39 -0600

    feat: Add que register routes

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import registerRoutes from './features/users/routes/register.routes';
 
 const app = express();
 const PORT = 3000;
@@ -6,6 +7,9 @@ const PORT = 3000;
 app.get('/', (req, res) => {
     res.send('Server is running on port 3000');
 });
+
+app.use(express.json());
+app.use('/api', registerRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
