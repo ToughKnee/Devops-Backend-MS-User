5339e1ea439999b44a8d7e08a5d81aca33eb4860
Johana Wu Nie <johana.wu@ucr.ac.cr>
2025-04-13 20:58:38 -0600

    fix: quit unused fields

--- src/features/users/services/getProfile.service.ts
+++ src/features/users/services/getProfile.service.ts
@@ -2,10 +2,8 @@
 import { AuthenticatedRequest } from '../../middleware/authenticate.middleware';
 
 export const getUserProfileService = (req: AuthenticatedRequest) => {
-  const { uid, email, role } = req.user!;
+  const { role } = req.user!;
   return {
-    uid,
-    email,
     role: role || 'USER' // Valor por defecto si no hay rol
   };
 };
